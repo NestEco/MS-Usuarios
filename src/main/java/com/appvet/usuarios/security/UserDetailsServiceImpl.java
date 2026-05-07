9821f3fd11ef493ac133ef5a635e31b294011ecd
NestEco <luc.gomezm@duocuc.cl>
2026-05-07 08:08:01 -0400

    Arreglo de archivos faltantes

---
+++ src/main/java/com/appvet/usuarios/security/UserDetailsServiceImpl.java
@@ -0,0 +1,30 @@
+package com.appvet.usuarios.security;
+
+import com.appvet.usuarios.repository.UsuarioRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.*;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UsuarioRepository usuarioRepository;
+
+    @Override
+    @Transactional(readOnly = true)
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return usuarioRepository.findByEmail(email)
+                .map(u -> new User(
+                        u.getEmail(),
+                        u.getPassword(),
+                        List.of(new SimpleGrantedAuthority("ROLE_" + u.getRol().toUpperCase()))
+                ))
+                .orElseThrow(() -> new UsernameNotFoundException(
+                        "Usuario no encontrado con email: " + email));
+    }
+}
